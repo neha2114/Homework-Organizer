ac74425b55f7ec35aa5f179a03d1ccdd41e660d6
neha2114 <neha.sapre2114@gmail.com>
2022-01-05 22:49:50 -0600

    displays post info for each assignment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [postList, setPostList] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8000/post").then((res) => {
+      setPostList(res.data);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        postList.map((value, key) => {
+            return (
+              <div className="postInfo">
+                <div className="title">{value.title}</div>
+                <div className="description">{value.description}</div>
+                <div className="duedate">{value.duedate}</div>
+                <div className="username">{value.username}</div>
+              </div>
+            );
+        })
+      }
     </div>
   );
 }
